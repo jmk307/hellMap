819f113ce5e4dfef1b107473955baad80f6726c3
강지민 <jimin112688@gmail.com>
2025-04-28 21:30:31 +0900

    Feat : 스프링부트 초기 세팅
    Spring Securtiy 5.x -> 6.x 업그레이드 및 초기 세팅

---
+++ hellMap/src/test/java/com/jimin/hellmap/HellMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimin.hellmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
